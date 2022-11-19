3fae02cded839d3bd975950f8939cfa5175fd9d9
Kevin Hurley <kphurley@meraki.com>
2022-11-19 13:37:11 -0600

    Working player and villain discard, villain card reveals

--- src/hooks/useBoardState.js
+++ src/hooks/useBoardState.js
@@ -10,9 +10,14 @@ import heroData from "../config/heroData.json";
 const initialState = {
   // A hash of card-ids to their state
   exhausted: {},
+
   // Special state for the hero, includes exhausted, flipped
   // Definitely will need stuff to capture multiple hero cards (Ironheart) or triple cards (Ant-Man, Wasp)
   hero: {},
+
+  // This is a hash for for villain cards.  Revealed means they are face up.
+  revealed: {},
+
   playerBoard: [],
   playerDeck: [],
   playerDiscard: [],
@@ -24,25 +29,59 @@ const initialState = {
   villainMainSchemes: [],
 }
 
-// action: { type: string, payload: string | array }
 function boardStateReducer(state, action) {
+  const updatedPlayerBoard = [...state.playerBoard];
+  const updatedPlayerDiscard = [...state.playerDiscard];
+  const updatedPlayerDeck = [...state.playerDeck];
+  const updatedVillainBoard = [...state.villainBoard];
+  const updatedVillainDeck = [...state.villainDeck];
+  const updatedVillainDiscard = [...state.villainDiscard];
+
   switch (action.type) {
+    case "DISCARD_CARD":
+      if (action.payload?.cardType === "player") {
+        const indexOfCardToRemove = updatedPlayerBoard.findIndex((card) => card.id === action.payload?.cardId)
+        if (indexOfCardToRemove < 0) {
+          return state;
+        }
+
+        const [removedCard] = updatedPlayerBoard.splice(indexOfCardToRemove, 1);
+        updatedPlayerDiscard.push(removedCard);
+      }
+      else if (action.payload?.cardType === "villain") {
+        const indexOfCardToRemove = updatedVillainBoard.findIndex((card) => card.id === action.payload?.cardId)
+        if (indexOfCardToRemove < 0) {
+          return state;
+        }
+
+        const [removedCard] = updatedVillainBoard.splice(indexOfCardToRemove, 1);
+        updatedVillainDiscard.push(removedCard);
+      };
+
+      return {
+        ...state,
+        playerDiscard: updatedPlayerDiscard,
+        playerBoard: updatedPlayerBoard,
+        villainDiscard: updatedVillainDiscard,
+        villainBoard: updatedVillainBoard,
+      };
     case "EXHAUST_CARD":
       return { ...state, exhausted: { ...state.exhausted, [action.payload]: true }};
-    case "READY_CARD":
-      return { ...state, exhausted: { ...state.exhausted, [action.payload]: false }};
+    case "FLIP_HERO":
+      return { ...state, hero: { ...state.hero, alterEgo: action.payload }};
     case "PLAYER_DRAW":
       if (state.playerDeck.length === 0) {
         return state;
       }
 
-      const updatedDeck = [...state.playerDeck];
-      const updatedBoard = [...state.playerBoard];
+      const drawnCard = updatedPlayerDeck.shift();
+      updatedPlayerBoard.push(drawnCard);
 
-      const drawnCard = updatedDeck.shift();
-      updatedBoard.push(drawnCard);
-
-      return { ...state, playerDeck: updatedDeck, playerBoard: updatedBoard };
+      return { ...state, playerDeck: updatedPlayerDeck, playerBoard: updatedPlayerBoard };
+    case "READY_CARD":
+      return { ...state, exhausted: { ...state.exhausted, [action.payload]: false }};
+    case "REVEAL_CARD":
+      return { ...state, revealed: { ...state.revealed, [action.payload]: true }};
     case "SHUFFLE_DECK":
       const shuffledDeck = arrayShuffle(state[action.payload])
       return { ...state, [action.payload]: shuffledDeck };
@@ -53,6 +92,7 @@ function boardStateReducer(state, action) {
       const formattedObligation = {
         id: obligation.octgn_id,
         name: obligation.name,
+        octgn_id: obligation.octgn_id,
       }
       tempVillainDeck.push(formattedObligation);
 
@@ -76,8 +116,15 @@ function boardStateReducer(state, action) {
     case "UPDATE_VILLAIN":
       const { villainCards, villainDeck, villainMainSchemes } = action.payload;
       return { ...state, villainCards, villainMainSchemes, villainDeck: [...state.villainDeck, ...villainDeck]};
-    case "FLIP_HERO":
-      return { ...state, hero: { ...state.hero, alterEgo: action.payload }};
+    case "VILLAIN_DRAW":
+      if (state.villainDeck.length === 0) {
+        return state;
+      }
+
+      const drawnVillainCard = updatedVillainDeck.shift();
+      updatedVillainBoard.push(drawnVillainCard);
+
+      return { ...state, villainDeck: updatedVillainDeck, villainBoard: updatedVillainBoard };
     default:
       return state;
   }
