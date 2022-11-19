602bcd75e11cbf299068a1f3e37403a49c95de87
Kevin Hurley <kphurley@gmail.com>
2022-11-19 11:31:31 -0600

    Move draw deck markup to common component (#20)
    Co-authored-by: Kevin Hurley <kphurley@meraki.com>

---
+++ src/components/TopNavigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav } from "rsuite";
+
+const TopNavigation = ({ openFileSelector, openStartGameModal }) =>
+  <Nav>
+    <Nav.Menu title="File">
+      <Nav.Item onClick={() => openFileSelector()}>Load Player Deck</Nav.Item>
+      <Nav.Item onClick={openStartGameModal}>Start Game</Nav.Item>
+    </Nav.Menu>
+  </Nav>
+
+export default TopNavigation;
