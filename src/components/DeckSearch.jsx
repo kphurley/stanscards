329f821ad97ea82fc5d9aa317889ec520280b1c1
Kevin Hurley <kphurley@meraki.com>
2022-11-23 15:39:42 -0600

    DeckSearch - Implement ability to move a card to a different place

--- src/components/DeckSearch.jsx
+++ src/components/DeckSearch.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import { Modal } from "rsuite";
+import { Modal, Nav, Popover, Whisper } from "rsuite";
 
 const DeckSearch = ({
   activeDeck,
   deckBeingSearched,
-  handleClose
+  dispatch,
+  handleClose,
 }) => {
+  const targetDeck = activeDeck && activeDeck.includes("villain") ? "villainBoard" : "playerBoard";
+
+  const getContextMenuForCardIndex = (idx) => (
+    <Popover arrow={false}>
+      <Nav vertical>
+        <Nav.Item onClick={() => dispatch({ type: "MOVE_CARD", payload: { idx, from: activeDeck, to: "villainDeck" }})}>
+          Move into encounter deck
+        </Nav.Item>
+        <Nav.Item onClick={() => dispatch({ type: "MOVE_CARD", payload: { idx, from: activeDeck, to: "playerDeck" }})}>
+          Move into player deck
+        </Nav.Item>
+        <Nav.Item onClick={() => dispatch({ type: "MOVE_CARD", payload: { idx, from: activeDeck, to: targetDeck }})}>
+          Put card into play
+        </Nav.Item>
+      </Nav>
+    </Popover>
+  )
+
   return (
     <Modal size="lg" open={!!activeDeck} onClose={handleClose}>
       <Modal.Header>
@@ -13,18 +32,20 @@ const DeckSearch = ({
       </Modal.Header>
       <Modal.Body>
         {
-          deckBeingSearched && deckBeingSearched.map((card) =>
-            <div
-              className="cardContainer"
-              data-id={card.id}
-              tabIndex="-1"
-            >
-              <img
-                className="card"
-                src={`/images/${card.octgn_id}.jpg`}
-                draggable="false"
-              />
-            </div>
+          deckBeingSearched && deckBeingSearched.map((card, idx) =>
+            <Whisper trigger="contextMenu" speaker={getContextMenuForCardIndex(idx)}>
+              <div
+                className="cardContainer cardInSearchContainer"
+                data-id={card.id}
+                tabIndex="-1"
+              >
+                <img
+                  className="card"
+                  src={`/images/${card.octgn_id}.jpg`}
+                  draggable="false"
+                />
+              </div>
+            </Whisper>
           )
         }
       </Modal.Body>
